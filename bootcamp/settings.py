1aa9670419073a269814a7d5eae337cd812650ef
siva-namala <siva7thworld@gmail.com>
2020-10-09 15:35:31 +0530

    added products app and templates folder

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'bootcamp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates", ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
