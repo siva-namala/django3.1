dca1aa784b344e5ab5a76d4083f0b681c8686177
siva-namala <siva7thworld@gmail.com>
2020-10-13 20:05:02 +0530

    added accounts app test cases(changed)

--- accounts/tests.py
+++ accounts/tests.py
@@ -14,9 +14,9 @@ class UserTestCase(TestCase):
         user.is_staff = True
         user.is_superuser = True
         user.save()
-        user.set_password(password)
+        user.set_password(password)    # saved after added password
+        user.save()
 
-        self.user_a = user
         self.password = password
 
     def test_user_qs_count(self):
@@ -30,16 +30,17 @@ class UserTestCase(TestCase):
         self.assertTrue(user_exists)
 
     def test_user_password(self):
-        self.assertTrue(self.user_a.check_password(self.password))
+        user = User.objects.get(username="siva")
+        self.assertTrue(user.check_password(self.password))
 
     def test_login_url(self):
         login_url = '/login/'
         data = {"username": "siva", "password": self.password}
         response = self.client.post(login_url, data, follow=True)
         status_code = response.status_code
-        url = response.request.get('PATH_INFO')
+        redirect_url = response.request.get('PATH_INFO')
 
         self.assertEqual(settings.LOGIN_URL, login_url)
         self.assertEqual(status_code, 200)
-        self.assertEqual(settings.LOGIN_URL, url)
+        self.assertEqual(settings.LOGIN_REDIRECT_URL, redirect_url)
 
