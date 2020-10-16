8b8e39ba42d5f08e46858e2666dfd5f5fd4030f9
siva-namala <siva7thworld@gmail.com>
2020-10-17 00:31:01 +0530

    added command for marking old orders as stale

--- orders/management/commands/order_update.py
+++ orders/management/commands/order_update.py
@@ -11,8 +11,8 @@ class Command(BaseCommand):
         now = timezone.now()
         today_start = now.replace(hour=0, minute=0, second=0,
                                   microsecond=0) - datetime.timedelta(days=2)
-        today_end= now.replace(hour=23, minute=59, second=59,
-                               microsecond=999999) - datetime.timedelta(days=2)
+        today_end = now.replace(hour=23, minute=59, second=59,
+                                microsecond=999999) - datetime.timedelta(days=2)
         qs = Order.objects.filter(timestamp__gte=today_start, timestamp__lte=today_end,
                                   status='created')
         qs.update(status='stale')
